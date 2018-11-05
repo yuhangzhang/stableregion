fd18eede94f9d409d7d98f328e12df140a12d2da
Yuhang Zhang <yuhang.zhang@live.com>
2018-11-05 14:47:33 +1100

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ imset = []
 
 for filename in os.listdir('/home/user/DATASETS/TRAIN_repository/VIC'):
     imset.append(Image.open(filename))
-    print(/home/user/DATASETS/TRAIN_repository/VIC/+file)
+    print('/home/user/DATASETS/TRAIN_repository/VIC/'+file)
     
 def kmeans(imset, k):
     pass
