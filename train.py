3d242880895466546bc2d4908f003825746246ff
Yuhang Zhang <yuhang.zhang@live.com>
2018-11-05 14:46:13 +1100

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 imset = []
 
-for filename in os.listdir('/home/user/DATASETS/TRAIN_repository/VIC):
+for filename in os.listdir('/home/user/DATASETS/TRAIN_repository/VIC'):
     imset.append(Image.open(file))
     print(file)
     
