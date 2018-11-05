5fe58a916ca5cf47d6f1932ec7c9ca3c8e406e25
Yuhang Zhang <yuhang.zhang@live.com>
2018-11-05 15:31:02 +1100

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 import os
+import sys
 import numpy as np
 from PIL import Image
 
 imset = []
 
 for filename in os.listdir('/home/user/DATASETS/TRAIN_repository/VIC'):
-    imset.append(Image.open('/home/user/DATASETS/TRAIN_repository/VIC/'+filename))
+    im = Image.open('/home/user/DATASETS/TRAIN_repository/VIC/'+filename).crop(0,0,1600,1000)
+    imset.append(np.array(im))
 
     
-def kmeans(imset, k):
+def kmeans(imset, k, maxiter=20):
     # initiate stable region
     stableregion = []
     for i in range(k):
@@ -19,9 +21,40 @@ def kmeans(imset, k):
     for i in range(k):
         membership.append([im for idx, im in enumerate(imset) if idx%k==i])
     
-    return membership
+    
+    for i in range(maxiter):
+        center = []
+        # with stableregion and membership, update center
+        for j in range(k):
+            average = np.zeros([1600,1000])
+            for im in membership[j]:
+                average = average + im*stabelregion[j]
+            average = average/len(membership[j])
+            center.append(average)
+        center = np.array(center)
+        
+        # with stableregion and center, update membership
+        membership = [[] for i in range(k)]
+        for im in imset:
+            assign = 0
+            mindis = sys.float_info.max
+            for j in range(k):
+                diff = center[j]-im*stableregion[j]
+                diff = np.fabs(diff).sum()
+                if diff<mindis:
+                    mindis = diff
+                    assign = j
+            membership[assign].append(im)
+            
+        membership = np.array(membership)
 
-
-m = kmeans(imset, 5)
-
-print(len(m[0]))
+        # with membership, update stableregion
+        variance = np.var(membership, axis=0)
+        for j in range(k):
+            median = np.median(variance[j])
+            stableregion[j] = variance<median
+    
+    return stableregion, membership
+            
+            
+            
